dff0f8535289e8b669c6611f6aec1955493a3c5f
dania-c <workingdania@hotmail.com>
2021-03-30 09:33:42 +0200

    step 4

--- index.js
+++ index.js
@@ -20,4 +20,31 @@ app.get('/', (req, res) => {
         message:`${t.getHours()+":" + t.getSeconds()}`
    }   )
   })
+
+
+//   app.get('/hello/:id', (req, res) => {
+//     res.send({
+//         status:200,
+//         message:"Hello , "+req.params.id
+//    }   )
+//   })
+app.get('/hello/:id', function (req, res) {
+    res.send({status:200, message:"Hello , "+req.params.id});
+  });
+
+
+  app.get('/search',function(req,res){
+
+    if(req.query.s){
+    res.send({status:200, message:"data : "+req.query.s});
+  
+  
+    }else{
+  
+      res.send( {status:500, error:true, message:"you have to provide a word to search for "});
+    }
+  
+  });
+
+
 app.listen(3000)
